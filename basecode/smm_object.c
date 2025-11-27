ce5360bb7f23e38c41ae28eef613c0a636707be5
helloyewon36 <helloyewon36@gmail.com>
2025-11-27 22:46:15 +0900

    new

---
+++ basecode/smm_object.c
@@ -0,0 +1,92 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODENR      100
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+
+
+
+
+static char smmObj_nodeName[MAX_NODETYPE][MAX_CHARNAME]={
+       
+        "lecture",
+        "restaurant",
+        "laboratory",
+        "home",
+        "gotoLab",
+        "foodchange",
+        "festival"
+};
+static int smmObj_nodeNr=0;
+
+typedef struct{
+       char name[MAX_CHARNAME];
+       int type;
+       int credit;
+       int energy;
+} smmObj_board_t;     
+       
+   
+           
+//struct instance array definition
+
+static smmObj_board_t_smmObj_board[MAX_NODENR]; 
+
+//object generation
+int  smmObj_genNode(char* name, int type, int credit, int energy)
+{
+     
+     strcpy(smmObj_board[smmObj_nodeNr].name, name);
+     smmObj_board[smmObj_nodeNr].type = type;
+     smmObj_board[smmObj_nodeNr].credit = credit;
+     smmObj_board[smmObj_nodeNr].energy = energy;
+     
+     smmObj_nodeNr++;
+    
+    return(smmObj_nodeNr);
+    
+}
+
+
+
+//member retrieving
+char* smmObj_getNodeName(int node_nr)
+{
+  return (smmObj_board[node_nr].name);     
+}
+int smmObj_getNodeType(int node_nr)
+{
+  return (smmObj_board[node_nr].type);     
+}
+int smmObj_getNodeCredit(int node_nr);
+{
+  return (smmObj_board[node_nr].credit);     
+}
+int smmObj_getNodeEnergy(int node_nr)
+{
+  return (smmObj_board[node_nr].energy);     
+}
+char* smmObj_getTypeName(int node_type)
+{
+  return (smmObj_nodeName[node_type]);     
+}
+
+#if 0
+
+
+
+char* smmObj_getGradeName(smmGrade_e grade)
+{
+    return smmGradeName[grade];
+}
+
+#endif
---
+++ basecode/smm_object.h
@@ -0,0 +1,57 @@
+//
+//  smm_object.h
+//  SMMarble object
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#ifndef smm_object_h
+#define smm_object_h
+
+/* node type :
+    lecture,
+    restaurant,
+    laboratory,
+    home,
+    experiment,
+    foodChance,
+    festival
+*/
+#define SMMNODE_TYPE_LECTURE      0
+#define SMMNODE_TYPE_RESTAURANT   1
+#define SMMNODE_TYPE_LABORATORY   2
+#define SMMNODE_TYPE_HOME         3
+#define SMMNODE_TYPE_GOTOLAB      4
+#define SMMNODE_TYPE_FOODCHANGE   5
+#define SMMNODE_TYPE_FESTIVAL     6
+
+/* grade :
+    A+,
+    A0,
+    A-,
+    B+,
+    B0,
+    B-,
+    C+,
+    C0,
+    C-
+
+
+*/
+
+//object generation
+int  smmObj_getNode(char* name, int type, int credit, int energy);
+char* smmObj_getNodeName(int node_nr);
+int smmObj_getNodeType(int node_nr);
+int smmObj_getNodeEnergy(int node_nr)
+char* smmObj_getTypeName(int node_type);
+int smmObj_getNodeCredit(int node_nr);
+
+//member retrieving
+
+
+//element to string
+
+
+
+#endif /* smm_object_h */
